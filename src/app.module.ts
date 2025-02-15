a18a5c818d6d0bb5961bf13dcfffd70953235a87
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-02-15 01:31:44 -0300

    add shared and core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PasswordsModule } from './passwords/passwords.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [PasswordsModule],
+  imports: [PasswordsModule, SharedModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
