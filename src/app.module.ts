7a8612f98efccdba1923e8035b002ffb5aa5d48c
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-02-15 01:54:21 -0300

    add validations; create users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PasswordsModule } from './passwords/passwords.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PasswordsModule, SharedModule, CoreModule],
+  imports: [PasswordsModule, SharedModule, CoreModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
