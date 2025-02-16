f26770e058dd9cea02d5091e512a25eb74ee1658
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-02-16 14:10:45 -0300

    fix config setup docker

--- prisma/migrations/20250216155427_set_account_email_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[accountEmail]` on the table `Password` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Password_accountEmail_key` ON `Password`(`accountEmail`);
