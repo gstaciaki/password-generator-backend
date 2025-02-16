565d69c44c4fb35fea666c45589441e6a616167b
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-02-16 00:58:59 -0300

    setup prisma and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PasswordsModule } from './passwords/passwords.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PasswordsModule, SharedModule, CoreModule, UsersModule],
+  imports: [PasswordsModule, SharedModule, CoreModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
